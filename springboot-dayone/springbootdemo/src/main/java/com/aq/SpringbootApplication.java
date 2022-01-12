2bee0eaa25115cb1b8f630d8b06eeeb7dbc8bc43
Aq <820668315@qq.com>
2022-01-12 13:27:23 +0800

    second-commit

---
+++ springboot-dayone/springbootdemo/src/main/java/com/aq/SpringbootApplication.java
@@ -0,0 +1,34 @@
+package com.aq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/******************************************************************************
+ * @Package: [com.aq.SpringbootApplication]
+
+ * @ClassName: [SpringbootApplication]
+
+ * @Description: [一句话描述该类的功能]
+
+ * @Author: [liang.wang@founder.com.cn]  
+
+ * @CreateDate: [2022/1/12 13:06]   
+
+ * @UpdateUser: [liang.wang@founder.com.cn (如多次修改保留历史记录，增加修改记录)]   
+
+ * @UpdateDate: [2022/1/12 13:06，(如多次修改保留历史记录，增加修改记录)]   
+
+ * @UpdateRemark: [说明本次修改内容, (如多次修改保留历史记录 ， 增加修改记录)]
+
+ * @Version: [v1.0]
+
+ * ${tags}
+
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+}
